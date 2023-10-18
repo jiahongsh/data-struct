78b2fb9ab16ef3ae3c9f4b12c607812503454ea0
一只贾 <3378079760@qq.com>
2023-10-18 07:32:26 +0000

    时间
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 时间复杂度的计算/test.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+
+int M = 0;
+     int N = 0;
+	int count = 0;
+	for (int k = 0; k < M; k++) {
+		++count;
+	}
+
+	for (int k = 0; k < N; k++) {
+		++count;
+	}
+	//大o的渐进表示法
+	//M远大于N->O（M）
+	//N远大与M->O(N)
+	//M和N差不多大->O(M) O(N)
+	return 0;
+}
