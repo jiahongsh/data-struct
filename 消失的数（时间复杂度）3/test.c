39f57c3694a8b243d0d3a25aaf1e76a954d5ebae
一只贾 <3378079760@qq.com>
2023-10-22 14:39:01 +0000

    最优解
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 消失的数（时间复杂度）3/test.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main() {
+	int arr[10] = { 0,2,4,6,1,8,7,5 };
+	int i = 0;
+	int x = 0;
+	for (i = 0; i <= 8; i++) {
+		x ^= i;
+	}
+	for (i = 0; i < 8; i++) {
+		x ^= arr[i];
+	}
+	printf("%d\n", x);
+}
+//该算法的时间复杂度为o(N),空间复杂度为O(1)
