99050c926b5738a1e9536ebca405a7d8f43ef15c
一只贾 <3378079760@qq.com>
2023-10-27 16:49:53 +0000

    最优时间复杂度
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 同类删除/test.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+int main() {
+	int arr[10] = { 1,1,2,2,2,3,3,3,4,4};
+	int*des = arr;
+	int* k = arr;
+	int* j = arr;
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		if (k == j) {
+			j++;
+			*des =* j;
+		}
+		else if (k != j) {
+			des++;
+			i = j;
+		}
+	}
+	return 0;
+}
+	//无建立新空间，空间复杂度为O（1）
+    //遍历一次数组，时间复杂度为O(N)
