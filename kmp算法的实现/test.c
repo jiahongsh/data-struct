e34b16f07ec615b41cafe4243426ecc66b75f6d7
一只贾 <3378079760@qq.com>
2023-11-29 15:34:15 +0000

    通过next简化时间复杂度
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ kmp算法的实现/test.c
@@ -0,0 +1,21 @@
+﻿//S为主串 T为子串
+int Index_kmp(Sstring S, SString T, int next[]) {
+	int j = 1;//子串的下标
+	int i = 1;//主串的下标
+	while (i <= S.length && i <= T.length) {
+		if (j == 0 || S.ch[i] == T.ch[j]) {
+			j++;
+			i++;
+		}
+		else {
+			//根据next数组j回到相应位置，避免j多次回溯浪费时间
+			j = next[j];
+		}
+
+		if (j > T.length) {
+			return i - T.length;//返回相同字串在主串位置的第一个字符的位置
+		}
+		//匹配失败，返回0
+		return 0;
+	}
+}
