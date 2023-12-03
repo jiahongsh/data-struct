11b045a28c0690f074bd236c05ffaaaa37169270
一只贾 <3378079760@qq.com>
2023-12-03 02:46:54 +0000

    线索化思路
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 二叉树线索化/test.c
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+
+typedef int TreeStyle;
+
+typedef struct ThreadNode {
+	TreeStyle data;
+	ThreadTree* lchild;
+	ThreadTree* rchild;
+	TreeStyle ltag;
+	TreeStyle rtag;
+}ThreadTree;
+
+ThreadTree * pre = NULL;//记录前置节点的变量
+void visit(ThreadTree* T) {
+	if (T->lchild == NULL) {
+		T->lchild = pre;
+		T->ltag = 1;
+	}
+	if (pre != NULL && T->rchild == NULL) {
+		T->rchild = pre;
+		T->rtag = 1;
+	}
+}
+//中序
+void InThread(ThreadTree* T){//相当与中序遍历这个树
+	if (T!= NULL) {
+		//根据左根右的方式
+		InThread(T->lchild);
+		visit(T);
+		InThread(T->rchild);
+
+	}
+}
+
+void CreateInThread(ThreadTree* T) {
+	if ( T== NULL) {
+		return 0;
+	}
+	//该树不为空树时，进行访问
+	InThread(T);
+}
+
