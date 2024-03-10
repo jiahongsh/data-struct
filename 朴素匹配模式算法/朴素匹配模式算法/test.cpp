b212b6bf72bc47dc4a392651b19bbc7415b64d48
a-jia_1_0 <3378079760@qq.com>
2024-03-11 00:03:41 +0800

    匹配算法

---
+++ 朴素匹配模式算法/朴素匹配模式算法/test.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+//默认数组的0位置不存数据
+//s1为主串
+//字符串的0位置存储字符串的长度
+int Index(string s1, string s2, int pos) {
+	int  i = pos;
+	s1[0] = s1.size();
+	s2[0] = s2.size();
+
+	int j = 1;
+	while (i <= s1[0] && j <= s2[0]) {
+		if (s1[i] == s2[j]) {
+			++i;
+			++j;
+		}
+		else {
+			i = i - j + 2;//返回下一个开始匹配的地方
+			j = 1;
+
+		}
+	}
+	if (j > s2[0]) {
+		return i - s2[0];//返回当前子串在主串中的起始位置；
+	}
+}
